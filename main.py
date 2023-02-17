1bed5443a58ed3d35a12f92926b05b2a32828390
SeanWilliams <Sean.Williams.3.sw@gmail.com>
2023-02-16 20:20:22 -0500

    Initial site load

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")  # goes to the home page
+def home_page():
+    #blog_url = "https://api.npoint.io/ec6c46fa70d4e754d079"
+    #response = requests.get(blog_url)
+    #all_posts = response.json()
+    #return render_template('index.html', blog_posts=all_posts)
+    return render_template('index.html')
+
+@app.route("/projects")
+def project_page():
+    return render_template("projects.html")
+
+@app.route("/post_article")
+def post_article_page():
+    return render_template("post_article.html")
+
+@app.route("/articles")
+def articles_page():
+    return render_template("articles.html")
+
+@app.route("/article")
+def article_page():
+    return render_template("article.html")
+
+@app.route("/about_us")
+def about_us_page():
+    return render_template("about_us.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form["username"]
+    password = request.form["password"]
+    return f"<h1>Name: {name} Password: {password}</h1>"
+
+@app.route("/post_article")
+def post_article():
+    return render_template("post_article.html")
+
+if __name__ == "__main__":  # is the same thing as calling the run function
+    app.run(host="0.0.0.0")
